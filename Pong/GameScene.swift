ffc2fcc9928cbbb0492df99505a9445e5ec15d34
Logan Schultz <schultzlogana@gmail.com>
2017-07-08 19:46:29 -0400

    Added paddles, ball, and collision

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -11,11 +11,42 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var ball = SKSpriteNode()
+    var enemyPaddle = SKSpriteNode()
+    var userPaddle = SKSpriteNode()
+    
     override func didMove(to view: SKView) {
-       
+        
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        enemyPaddle = self.childNode(withName:"enemyPdl") as! SKSpriteNode
+        userPaddle = self.childNode(withName: "userPdl") as! SKSpriteNode
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            userPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            userPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        enemyPaddle.run(SKAction.moveTo(x: ball.position.x, duration: 1))
     }
 }
