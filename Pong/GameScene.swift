35388d428c6a44d6fd4136a7469bcd416dd73020
Logan Schultz <schultzlogana@gmail.com>
2017-07-10 13:27:54 -0400

    Finished game, added icons

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -15,11 +15,19 @@ class GameScene: SKScene {
     var enemyPaddle = SKSpriteNode()
     var userPaddle = SKSpriteNode()
     
+    var userScore = SKLabelNode()
+    var enemyScore = SKLabelNode()
+    
+    var score = [Int]()
+    
     override func didMove(to view: SKView) {
         
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         enemyPaddle = self.childNode(withName:"enemyPdl") as! SKSpriteNode
         userPaddle = self.childNode(withName: "userPdl") as! SKSpriteNode
+        userScore = self.childNode(withName: "userScore") as! SKLabelNode
+        enemyScore = self.childNode(withName: "enemyScore") as! SKLabelNode
+        
         
         ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
         
@@ -30,6 +38,26 @@ class GameScene: SKScene {
         
         self.physicsBody = border
         
+        startGame()
+        
+    }
+    
+    func startGame() {
+        score = [0,0]
+    }
+    
+    func addScore(playerWhoWon: SKSpriteNode) {
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+
+        
+        if playerWhoWon == userPaddle {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        } else {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -47,6 +75,15 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        enemyPaddle.run(SKAction.moveTo(x: ball.position.x, duration: 1))
+        enemyPaddle.run(SKAction.moveTo(x: ball.position.x, duration: 0.5))
+        
+        userScore.text = "\(score[0])"
+        enemyScore.text = "\(score[1])"
+        
+        if ball.position.y <= userPaddle.position.y - 30 {
+            addScore(playerWhoWon: enemyPaddle)
+        } else if ball.position.y >= enemyPaddle.position.y + 30 {
+            addScore(playerWhoWon: userPaddle)
+        }
     }
 }
